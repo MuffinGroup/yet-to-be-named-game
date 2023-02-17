8acff39790b8a3d626f31ce23da71166ad811eed
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-17 21:52:26 +0100

    Inital commit

--- src/main/common/yet_to_be_named_game.py
+++ src/main/common/yet_to_be_named_game.py
@@ -1,10 +1,15 @@
-import pygame
+#Data types
+#Strings
+String = "Hello world" #Strings store letters and words
+Integer = 1.999999999 #Integers store numbers which can also be used for mathematical operations
+Boolean = True #Booleans store a value that has to be "True" or "False"
 
-pygame.init
-screen = pygame.display.set_mode((800, 400))
-a = ["www", "eee"]
-print(a)
-hello
+#The basics
+#printing String
+print("Hello World") #Prints "Hello World to the console"
+print(String) #Prints "Hello World" which has been defined in the String called "String"
 
-def hello():
-    print("ok")
+#Printing Integers
+print(1.999999999) #Prints 1.999999999 to the console
+print(Integer) #Prints the value that has been assigned to the integer called "Integer" to the console
+
