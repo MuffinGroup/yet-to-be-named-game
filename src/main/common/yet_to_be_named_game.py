c53378f5d95f0b1690f349fba0e6110506c774a5
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-17 21:58:57 +0100

    Finished python basics

--- src/main/common/yet_to_be_named_game.py
+++ src/main/common/yet_to_be_named_game.py
@@ -13,3 +13,13 @@ print(String) #Prints "Hello World" which has been defined in the String called 
 print(1.999999999) #Prints 1.999999999 to the console
 print(Integer) #Prints the value that has been assigned to the integer called "Integer" to the console
 
+#maths
+print(3+1) #This will output the result of this addition
+print(Integer + 1) #It works with Integers as well!
+print(Integer - 1) #subtraction
+print(Integer * 1) #multiplication
+print(Integer / 1) #division
+
+#Printing Booleans
+print(True) #This will output the value "True"
+print(Boolean) #This will output the value assigned to it (in this case "True" this makes it more dynamic than simply printing a certain value)
