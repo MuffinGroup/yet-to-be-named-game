5bb20758c8e94f95dc7dd647815518e6b79a7226
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-19 17:05:43 +0100

    update inputs

--- src/main/common/python_knowlege_base.py
+++ src/main/common/python_knowlege_base.py
@@ -1,14 +1,13 @@
-#Data types (variables)
-
 #Important info: in python you start counting from 0
 
+#1. Data types (variables)
 #Strings
-String = "Hello world" #Strings store letters and words
+String = "Hello world" #Strings (str) store letters and words
 Integer = 1.999999999 #Integers (int) store numbers which can also be used for mathematical operations
-Boolean = True #Booleans store a value that has to be "True" or "False"
+Boolean = True #Booleans (bool) store a value that has to be "True" or "False"
 Array = ["String", 3, True] #An array stores different datataypes
 
-#The basics
+#2. The basics
 #printing Strings
 print("Hello World") #Prints "Hello World to the console"
 print(String) #Prints "Hello World" which has been defined in the String called "String"
@@ -42,7 +41,7 @@ print(Array[0]) #This will print the first array entry
 print(Array[1]) #This will print the second array entry
 print(Array[2]) #This will print the third array entry
 
-#if statements (logical operators)
+#3. if statements (logical operators)
 a = 1
 b = 2 #Declaring some variables
 
@@ -75,12 +74,12 @@ if a < b: #Instead of using "and" you can also put two if statements inside of e
     if c < d: 
         print("Both conditions are met") 
 
-#Input
+#4. Input
 #After looking at how to output code, we will take a look at a simple way to read user input
-question1 = input("What is your name?") #This will print a string and wait for user input. As soon as the user has typed his answer into the terminal and has pressed "Enter", the information will be stored and printed out in the next line
+question1 = input("What is your name?") #This will print a string and wait for user input (the input is a string by default so booleans and ints will be ignored). As soon as the user has typed his answer into the terminal and has pressed "Enter", the information will be stored and printed out in the next line
 print("Hello " + question1) #This will output the stored input
 
-#negative and positive if statements
+#3. negative and positive if statements
 #if the input is "t" prints out "Your name is t"
 if question1 is "t": #the "is" can be replaced with "==" as well
     print("Your name is t")
@@ -94,6 +93,16 @@ if question1 == "0": #the "==" can be replaced with "is" as well
 else:
     print("Your name is not 0")
 
+#Inputs that accept different values
+question2 = input("How old are you? ") #Prints out "How old are you?" and waits for user input
+question2 = int(question2) #Converts the input into an integer (allows for better mathematical interaction)
+
+question2 = input("How old are you? ") #Prints out "How old are you?" and waits for user input
+question2 = bool(question2) #Converts the input into an integer (allows for better mathematical interaction)
+
+question2 = input("How old are you? ") #Prints out "How old are you?" and waits for user input
+question2 = (question2) #Converts the input into an integer (allows for better mathematical interaction)
+
 #While loops
 #while(True):    #This will infinetly print because the value always is "True"
 #    print("it never stops...")
@@ -101,6 +110,6 @@ else:
 #while(a < b):   #This wont stop either because a always is smaller than b. That means the output boolean is always "True"
 #    print("it does not stop either")
 
-#for loops
+#5. for loops (me is too dumb to figure this out...)
 for i in Array: #prints an entire array. I dont understand this pls help
     print(i)
