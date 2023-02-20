5be7b41db8aa2f1dd4688c632327fad85fe6ced7
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-20 15:22:33 +0100

    added exit checker

--- src/main/common/Felix/start/start.py
+++ src/main/common/Felix/start/start.py
@@ -3,11 +3,12 @@ import pygame
 pygame.init
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("test")
-pygame.display
-
+clock = pygame.time.Clock()
 while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print("closed window successfully")
                 pygame.quit
                 exit()
         pygame.display.update()
+        clock.tick(60)
