6e5be558d547ea9064f2d3a28f2bcd19487014f4
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-20 15:31:42 +0100

    moved to personal start folder

---
+++ src/main/common/Benedikt/start/start.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("test")
+clock = pygame.time.Clock()
+img = pygame.image.load('C:\Users\Admin\python\yet-to-be-named-game\src\main\common\Benedikt\start\icon.png')
+pygame.display.set_icon(img)
+
+while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Closed succesful")
+                pygame.quit
+                exit()
+        pygame.display.update()
+        clock.tick(60)
