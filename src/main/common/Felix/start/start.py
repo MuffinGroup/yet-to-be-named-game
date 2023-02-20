241d5763a976f0e58b7c58bdccb4021c158a23f5
TheHackerChampion <felix.schweikl@hgtb.de>
2023-02-20 15:43:16 +0100

    sync

--- src/main/common/Felix/start/start.py
+++ src/main/common/Felix/start/start.py
@@ -4,10 +4,13 @@ pygame.init
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("test")
 clock = pygame.time.Clock()
+img = pygame.image.load('C:\Users\andre\OneDrive - Hertzhaimer Gymnasium Trostberg\Eigenes und Zuhause\Programmieren\yet-to-be-named-game\src\main\common\Felix\start\icon.png')
+pygame.display.set_icon(img)
 
 while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print("Closed succesful")
                 pygame.quit
                 exit()
         pygame.display.update()
