2be2323d991042abcd48b83788899a51f04e7ba1
TheHackerChampion <felix.schweikl@hgtb.de>
2023-02-20 15:38:51 +0100

    sync

--- src/main/common/Felix/start/start.py
+++ src/main/common/Felix/start/start.py
@@ -1,8 +1,10 @@
+import pygame
 
 pygame.init
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("test")
 clock = pygame.time.Clock()
+
 while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
