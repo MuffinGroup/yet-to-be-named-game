96cd0d195a743942e5c346f12fca8cbaf276dc1a
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-20 15:45:37 +0100

    Fixed icon

--- src/main/common/Felix/start/start.py
+++ src/main/common/Felix/start/start.py
@@ -4,7 +4,7 @@ pygame.init
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("test")
 clock = pygame.time.Clock()
-img = pygame.image.load('C:\Users\andre\OneDrive - Hertzhaimer Gymnasium Trostberg\Eigenes und Zuhause\Programmieren\yet-to-be-named-game\src\main\common\Felix\start\icon.png')
+img = pygame.image.load('src//main//assets//icon//icon.png')
 pygame.display.set_icon(img)
 
 while True:
