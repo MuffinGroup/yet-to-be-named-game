e658317e0926d00204edee2319087d20daedca1f
TheHackerChampion <felix.schweikl@hgtb.de>
2023-02-20 15:17:18 +0100

    sync

--- src/main/common/Felix/start/start.py
+++ src/main/common/Felix/start/start.py
@@ -6,7 +6,7 @@ screen = pygame.display.set_mode((800, 400))
 
 while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: 
                 pygame.quit
 
         pygame.display.update()
