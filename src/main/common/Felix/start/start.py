eab8af6d0d2844f66b3e1b9a026a16e9cc87487f
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-20 15:12:26 +0100

    added caption

--- src/main/common/Felix/start/start.py
+++ src/main/common/Felix/start/start.py
@@ -2,11 +2,10 @@ import pygame
 
 pygame.init
 screen = pygame.display.set_mode((800, 400))
-
+pygame.display.set_caption("test")
 
 while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit
-
         pygame.display.update()
