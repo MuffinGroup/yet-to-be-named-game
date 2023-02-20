ff574f00155fca930d1703d4db5f7715c4b711ca
TheHackerChampion <felix.schweikl@hgtb.de>
2023-02-20 15:29:24 +0100

    update

--- src/main/common/Felix/start/start.py
+++ src/main/common/Felix/start/start.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("test")
-pygame.display
+clock = pygame.time.Clock()
 
 while True:
         for event in pygame.event.get():
