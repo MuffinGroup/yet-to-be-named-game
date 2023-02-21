9862aa77633a03327d52a2541b9ddc57c4c3f142
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-21 22:29:32 +0100

    Started setting up entity registry

---
+++ src/main/common/Benedikt/enemies/Background.py
@@ -0,0 +1,48 @@
+#Importieren u.initialisieren der Pygame-Bibliothek
+import pygame
+pygame.init()
+
+#Variablen/KONSTANTEN setzen
+W, H= 800,600
+FPS=60
+SCHWARZ=(0,0,0)
+WEISS  =(255,255,255)
+GRAU   =(155,155,155)
+spielaktiv=True
+
+#gegner
+gegnerBILD=pygame.image.load("src\main\common\Lukas\Pictures\Background final2.jpg")
+bildgroessen=gegnerBILD.get_rect()
+print(bildgroessen)
+print(bildgroessen.center[0])
+print(bildgroessen.center[1])
+print(bildgroessen.width)
+print(bildgroessen.height)
+
+#Definieren und Öffnen eines neuen Fensters
+fenster=pygame.display.set_mode((W,H))
+pygame.display.set_caption("Grafiken nutzen")
+clock=pygame.time.Clock()
+
+#Schleife Hauptprogramm
+while spielaktiv:
+    #Überprüfen, ob Nutzer eine Aktion durchgeführt hat
+    for event in pygame.event.get():
+        #beenden bei [ESC] oder [X]
+        if event.type==pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE):
+            spielaktiv=False
+
+    #Spielfeld/figuren zeichnen
+    fenster.blit(gegnerBILD,(0,0))
+    
+    #Fenster aktualisieren
+    pygame.display.flip()
+    clock.tick(FPS)
+    
+    #Spielfeld löschen
+    fenster.fill(GRAU)
+
+    
+
+
+
---
+++ src/main/common/Benedikt/enemies/Character Test.py
@@ -0,0 +1,49 @@
+import pygame
+from colors import *
+
+pygame.init()
+
+# Set screen dimensions
+screen_width = 640
+screen_height = 480
+
+# Create a screen surface
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Load character image
+character_image = pygame.image.load("src//main//assets//entities//enemy1.png").convert_alpha()
+
+# Set initial position
+character_x = screen_width // 6
+character_y = screen_height // 6
+
+# Set character speed
+character_speed = 0.5
+
+# Game loop
+running = True
+while running:
+
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # Handle keyboard input
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        character_x -= character_speed
+    if keys[pygame.K_RIGHT]:
+        character_x += character_speed
+    if keys[pygame.K_UP]:
+        character_y -= character_speed
+    if keys[pygame.K_DOWN]:
+        character_y += character_speed
+
+    # Draw the character and update the screen
+    screen.fill(COLORS.BLUE)
+    screen.blit(character_image, (character_x, character_y))
+    pygame.display.update()
+
+# Quit Pygame
+pygame.quit()
