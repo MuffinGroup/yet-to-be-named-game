8284ea0e6512d05322fac9c1fe811c66609b1f17
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-21 16:35:42 +0100

    Code cleanup

--- src/main/common/Benedikt/start/start.py
+++ src/main/common/Benedikt/start/start.py
@@ -4,24 +4,21 @@ from colors import *
 
 pygame.init()
 mouse = pygame.mouse.get_pos
-res = (720,720)
-screen = pygame.display.set_mode(res)
-color = (255,255,255)
+screen = pygame.display.set_mode((720,720))
 color_light = (COLORS.RED)
 color_dark = (100,100,100)
 width = screen.get_width()
 height = screen.get_height()
 smallfont = pygame.font.SysFont('Corbel',35)
-start = smallfont.render('Start Game' , True , color)
-quit = smallfont.render('Quit Game' , True , color)
+start = smallfont.render('Start Game' , True , COLORS.WHITE)
+quit = smallfont.render('Quit Game' , True , COLORS.WHITE)
 icon = pygame.image.load('src//main//assets//icon//icon.png')
 icon_selected = pygame.image.load('src//main//assets//icon//icon_selected.png')
 pygame.display.set_icon(icon)
 start_button = registerButton.Button(100, 100, icon, icon_selected, 0.8)
 clock = pygame.time.Clock()
 
-run = True
-while run:
+while True:
 
 	screen.fill(COLORS.GRAY)
 
@@ -33,11 +30,9 @@ while run:
 
 	#event handler
 	for event in pygame.event.get():
-		#quit game
 		if event.type == pygame.QUIT:
-			run = False
-
+			pygame.quit
+			exit()
+			print("Closed successful")
 	pygame.display.update()
-
-pygame.quit()
     #screen.blit(img, (0, 0))
