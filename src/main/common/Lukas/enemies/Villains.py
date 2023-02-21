1a50875027f785dca1d374745b230ca153bbd003
ReadyPlayerOne14 <anitaseidl@outlook.de>
2023-02-21 13:58:56 +0100

    Modifications reset

--- src/main/common/Lukas/enemies/Villains.py
+++ src/main/common/Lukas/enemies/Villains.py
@@ -1,7 +1,5 @@
 #Importieren u.initialisieren der Pygame-Bibliothek
 import pygame
-from util.colors.colors import *
-from pygame.locals import*
 pygame.init()
 
 #Variablen/KONSTANTEN setzen
@@ -13,7 +11,7 @@ GRAU   =(155,155,155)
 spielaktiv=True
 
 #gegner
-gegnerBILD=pygame.image.load("src\main\common\Lukas Villans\Pictures\Happy Halloween_Copy 3.1.png")
+gegnerBILD=pygame.image.load("src\main\common\Lukas\Pictures\Happy Halloween_Copy 3.1.png")
 bildgroessen=gegnerBILD.get_rect()
 print(bildgroessen)
 print(bildgroessen.center[0])
@@ -31,7 +29,7 @@ while spielaktiv:
     #Überprüfen, ob Nutzer eine Aktion durchgeführt hat
     for event in pygame.event.get():
         #beenden bei [ESC] oder [X]
-        if event.type==QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE):
+        if event.type==pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE):
             spielaktiv=False
 
     #Spielfeld/figuren zeichnen
@@ -42,7 +40,7 @@ while spielaktiv:
     clock.tick(FPS)
     
     #Spielfeld löschen
-    fenster.fill(COLORS.GRAY)
+    fenster.fill(GRAU)
 
     
 
