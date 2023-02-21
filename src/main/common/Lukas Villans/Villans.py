878b9894242eec6655ee9a1a68b78229b74db154
ReadyPlayerOne14 <anitaseidl@outlook.de>
2023-02-21 12:01:13 +0100

    Code to open Pictures

---
+++ src/main/common/Lukas Villans/Villans.py
@@ -0,0 +1,49 @@
+#Importieren u.initialisieren der Pygame-Bibliothek
+import pygame
+from pygame.locals import*
+pygame.init()
+
+#Variablen/KONSTANTEN setzen
+W, H= 800,600
+FPS=60
+SCHWARZ=(0,0,0)
+WEISS  =(255,255,255)
+GRAU   =(155,155,155)
+spielaktiv=True
+
+#gegner
+gegnerBILD=pygame.image.load("src\main\common\Lukas Villans\Pictures\Happy Halloween_Copy 3.1.png")
+bildgroessen=gegnerBILD.get_rect()
+print(bildgroessen)
+print(bildgroessen.center[0])
+print(bildgroessen.center[1])
+print(bildgroessen.width)
+print(bildgroessen.height)
+
+#Definieren und Öffnen eines neuen Fensters
+fenster=pygame.display.set_mode((W,H))
+pygame.display.set_caption("Grafiken nutzen")
+clock=pygame.time.Clock()
+
+#Schleife Hauptprogramm
+while spielaktiv:
+    #Überprüfen, ob Nutzer eine Aktion durchgeführt hat
+    for event in pygame.event.get():
+        #beenden bei [ESC] oder [X]
+        if event.type==QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE):
+            spielaktiv=False
+
+    #Spielfeld/figuren zeichnen
+    fenster.blit(gegnerBILD,(0,0))
+    
+    #Fenster aktualisieren
+    pygame.display.flip()
+    clock.tick(FPS)
+    
+    #Spielfeld löschen
+    fenster.fill(GRAU)
+
+    
+
+
+
