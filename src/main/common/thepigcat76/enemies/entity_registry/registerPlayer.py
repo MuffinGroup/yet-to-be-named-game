427942d3f8d3d95d4c82f71fdf3c502a036ef657
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-23 10:27:06 +0100

    Deleted a bunch of folders

--- src/main/common/thepigcat76/enemies/entity_registry/registerPlayer.py
+++ src/main/common/thepigcat76/enemies/entity_registry/registerPlayer.py
@@ -1,8 +1,8 @@
 import pygame
 
 class player():
-	def __init__(self, enemy_name, x, y, scale):
-		image = pygame.image.load('src/main/assets/characters/Character1/Animations/' + enemy_name + '.png')
+	def __init__(self, player_name, x, y, scale):
+		image = pygame.image.load('src/main/assets/characters/Character1/Animations/' + player_name + '.png')
 		width = image.get_width()
 		height = image.get_height()
 		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
@@ -11,9 +11,8 @@ class player():
 		self.clicked = False
 
 	def draw(self, surface):
+		#Will be executred during the while loop
 		action = False
-		#get mouse position
 		pos = pygame.mouse.get_pos()
 		surface.blit(self.image, (self.rect.x, self.rect.y))
-		return action	
-
+		return action
