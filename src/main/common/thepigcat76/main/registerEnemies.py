ecbd8dec9e78a9f314ec56311ce2f2ae46407f13
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-25 11:34:51 +0100

    Improving my commit count

--- src/main/common/thepigcat76/main/registerEnemies.py
+++ src/main/common/thepigcat76/main/registerEnemies.py
@@ -2,7 +2,7 @@ import pygame
 
 class enemies():
 	def __init__(self, enemy_name, x, y, scale):
-		image = pygame.image.load('src//main//assets//entities//' + enemy_name + '.png')
+		image = pygame.image.load('src/main/assets/entities/enemies/' + enemy_name + '.png')
 		width = image.get_width()
 		height = image.get_height()
 		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
