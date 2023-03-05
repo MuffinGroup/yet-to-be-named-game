1f9f7ce7d04f845c231fdb1b308747bf629e3ae4
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-03-05 17:08:07 +0100

    Animations mostly working. So much pain...

--- src/main/common/thepigcat76/main/doorTest.py
+++ src/main/common/thepigcat76/main/doorTest.py
@@ -1,6 +1,6 @@
 import pygame
 from colors import *
-
+import animations
 
 # Initialize Pygame
 pygame.init()
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 #Load background
 background = pygame.image.load("src/main/assets/elements/background/hallway.jpg")
 floor = pygame.image.load("src\main/assets/elements/background/floor.jpg")
-door = pygame.image.load("src/main/assets/elements/doors/door_1_closed.png")
+door = pygame.image.load("src\main/assets\elements\doors\Door_1_closed_Überarbeitet.png")
 
 characterWidth = 32
 characterHeight = 32
@@ -25,20 +25,8 @@ idle_sprite5 = pygame.image.load("src/main/assets/entities/characters/Character1
 idle_sprite6 = pygame.image.load("src/main/assets/entities/characters/Character1/Animations/Character1Idle/1Idle(7).png")
 idle_sprite7 = pygame.image.load("src/main/assets/entities/characters/Character1/Animations/Character1Idle/1Idle(8).png")
 
-walking_sprite0 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(1).png")
-walking_sprite1 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(2).png")
-walking_sprite2 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(3).png")
-walking_sprite3 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(4).png")
-walking_sprite4 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(5).png")
-walking_sprite5 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(6).png")
-walking_sprite6 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(7).png")
-walking_sprite7 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(8).png")
-walking_sprite8 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(9).png")
-walking_sprite9 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(10).png")
-walking_sprite10 = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1running/1Running(11).png")
 
-idle_sprite = [idle_sprite0, idle_sprite1, idle_sprite2, idle_sprite3, idle_sprite4, idle_sprite5, idle_sprite6, idle_sprite7]
-walking_sprite = [walking_sprite0, walking_sprite1, walking_sprite2, walking_sprite3, walking_sprite4, walking_sprite5, walking_sprite6, walking_sprite7, walking_sprite8, walking_sprite9, walking_sprite10]
+
 # Set screen dimensions
 scale = 10
 
@@ -56,7 +44,7 @@ rightWall = pygame.draw.rect(screen, (0,0,0), (1100,0,2,1000), 0)
 
 #Create Sound
 jumpsound = pygame.mixer.Sound("src/main/assets/sounds/entities/jump.wav")
-jumpsound.set_volume(0.25)
+jumpsound.set_volume(0.01)
 doorsound = pygame.mixer.Sound("src\main/assets\sounds\entities\Door_Closing.wav")
 
 # Load character image
@@ -70,7 +58,7 @@ character_image = pygame.transform.scale(character_image, (int(image_width * sca
 character_image_inverted = pygame.transform.scale(character_image_inverted, (int(image_width * scale), int(image_height * scale)))
 door_width = door.get_width()
 door_height = door.get_height()
-door = pygame.transform.scale(door, (int(door_width * scale), int(door_height * scale)))
+door = pygame.transform.scale(door, (int(door_width * scale/2), int(door_height * scale/2)))
 currentSprite = character_image
 # Sizes door: 320, 320
 
@@ -98,6 +86,7 @@ doorhandling = 0
 visible = True
 standing = True
 walking = False
+jumping = False
 while running:
     clock.tick(180)
     # Handle events
@@ -106,13 +95,13 @@ while running:
             running = False
             pygame.quit()
 
-    if value >= len(idle_sprite):
+    if value >= len(animations.idle_sprite):
         value = 0
 
-    currentSprite = idle_sprite[value]
+    currentSprite = animations.idle_sprite[value]
 
-    if value >= len(walking_sprite):
-        value = 0
+    if WalkingValue >= len(animations.walking_sprite):
+        WalkingValue = 0
     
     # Handle keyboard input
     keys = pygame.key.get_pressed()
@@ -124,16 +113,21 @@ while running:
         character_x -= character_speed
     else:
         standing = True
+        walking = False
     if keys[pygame.K_RIGHT] or keys[pygame.K_d] and not Spieler.colliderect(rightWall) and visible == True:
         standing = False
         walking = True
         character_x += character_speed
     else:
         standing = True
+        walking = False
     if keys[pygame.K_UP] and jumpvar == -16 and visible == True:
         standing = False
+        jumping = True
         jumpvar = 15
     elif keys[pygame.K_SPACE] and jumpvar == -16 and visible == True:
+        standing = False
+        jumping = True
         jumpvar = 15
     if keys[pygame.K_DOWN] or keys[pygame.K_s] and Spieler.colliderect(Door) and visible == True:
         doorhandling = 1
@@ -150,7 +144,10 @@ while running:
 
     if doorhandling == 1:
         door = pygame.image.load("src/main/assets/elements/doors/door_1_open.png")
-        door = pygame.transform.scale(door, (int(door_width * scale), int(door_height * scale)))
+        door = pygame.transform.scale(door, (int(door_width * scale/2), int(door_height * scale/2)))
+
+    if walking == True:
+        currentSprite = animations.walking_sprite[WalkingValue]
 
     draw()
     currentSprite = pygame.transform.scale(currentSprite, (int(characterWidth * scale), int(characterHeight * scale)))    
@@ -161,17 +158,15 @@ while running:
     if doorhandling == 1:
         pygame.time.wait(500)
         pygame.mixer.Sound.play(doorsound)
-        door = pygame.image.load("src/main/assets/elements/doors/door_1_closed.png")
-        door = pygame.transform.scale(door, (int(door_width * scale), int(door_height * scale)))
+        door = pygame.image.load("src\main/assets\elements\doors\Door_1_closed_Überarbeitet.png")
+        door = pygame.transform.scale(door, (int(door_width * scale/2), int(door_height * scale/2)))
         visible = False
         doorhandling = 0
 
     if standing == True:
-        pygame.time.delay(180)
         value += 1
 
     if walking == True:
-        pygame.time.delay(180)
         WalkingValue += 1
 
     clock.tick(69)
