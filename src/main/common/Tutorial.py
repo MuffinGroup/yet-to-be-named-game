6e44736e487c1db578f62c7542dbcd3120a65f0c
ReadyPlayerOne14 <anitaseidl@outlook.de>
2023-03-06 20:13:43 +0100

    ExitByESC

--- src/main/common/Tutorial.py
+++ src/main/common/Tutorial.py
@@ -98,7 +98,7 @@ while running:
     clock.tick(180)
     # Handle events
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE):
             running = False
             pygame.quit()
 
