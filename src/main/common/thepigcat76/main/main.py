560b6a5b1b40fc526c4336fde03b97114e24cbce
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-03-07 16:04:41 +0100

    Finished variables, started working on game loop

--- src/main/common/thepigcat76/main/main.py
+++ src/main/common/thepigcat76/main/main.py
@@ -1,5 +1,6 @@
 import pygame
 import colors
+import animations
 
 class main():
     #Pygame initialization
@@ -27,7 +28,29 @@ class main():
     #Screen dimensions
     screen_width = 1280
     screen_height = 800
-    
+
     # Set initial position
     character_x = 150
     character_y = 410
+    
+    # Set character speed
+    character_speed = 5
+    
+    #Values for animations
+    IdleValue = 0
+    WalkingValue = 0
+    
+    # Game loop
+    running = True
+
+    #Character attributes
+    jumpvar = -16
+    doorhandling = 0
+    visible = True
+    standing = True
+    walking = False
+    jumping = False
+
+    #Game loop
+    while running:
+        
