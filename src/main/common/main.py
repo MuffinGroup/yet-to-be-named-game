ca9c3755fa3d61b54cb36743802e9c40de294b2f
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-03-16 17:38:31 +0100

    animations and console output work

--- src/main/common/main.py
+++ src/main/common/main.py
@@ -3,31 +3,35 @@ import colors
 import animations
 
 pygame.init()
-#
-###
+
 class Player:
     def __init__(currentImage):
-        Player.rightImage = pygame.image.load("src\main/assets\entities\characters\Character1\Animations\Character1.png") # Create Player Image
-        Player.leftImage = pygame.transform.flip(Player.rightImage, True, False)
-        Player.jumpsound = pygame.mixer.Sound("src/main/assets/sounds/entities/jump.wav")
+        Player.rightImage = pygame.image.load("src\main/assets/textures\entities\characters\character_1/animations\character_1.png") # Create Player Image
+        Player.jumpsound = pygame.mixer.Sound("src/main/assets/sounds/jump.wav")
         Player.jumpsound.set_volume(0.25)
-        Player.currentImage = Player.rightImage
+        Player.currentSprite = Player.rightImage
         Player.image_speed= 5 
         Player.jumpvar = -16
+        Player.standingRight = True
+        Player.standingLeft = False
         Player.standing = True
+        Player.jumpingRight = False
+        Player.jumpingLeft = False
         Player.jumping = False
+        Player.walkingRight = False
+        Player.walkingLeft = False
         Player.walking = False
         Player.rect = pygame.Rect((50,50),(30,30)) # Create Player Rect
+
     def move(self,camera_pos):
         self.running = True
         self.doorhandling = 0
         self.visible = True
         pos_x,pos_y = camera_pos # Split camara_pos
-        #
+        
         key = pygame.key.get_pressed() # Get Keyboard Input
         if key[pygame.K_UP] and Player.jumpvar == -16 and self.visible == True: # Check Keyif keys[pygame.K_UP] and jumpvar == -16 and visible == True:
             Player.standing = False
-            Player.jumping = True
             Player.jumpvar = 15
         elif key[pygame.K_SPACE] and Player.jumpvar == -16 and self.visible == True:
             Player.jumpvar = 15
@@ -41,27 +45,27 @@ class Player:
                 n = -1
             self.rect.y -= (Player.jumpvar**2)*0.17*n
             pos_y += (Player.jumpvar**2)*0.17*n
+            Player.jumping = True
             Player.jumpvar -= 1
 
         if key[pygame.K_LEFT] and self.visible == True:
+            Player.walkingLeft = True
             Player.standing = False
-            Player.walking = True
-            Player.currentImage = Player.leftImage
+            Player.standingLeft = True
             self.rect.x -= Player.image_speed 
             pos_x += Player.image_speed
         else:
-            Player.standing = True
-            Player.walking = False
+            Player.walkingLeft = False
+
         if key[pygame.K_RIGHT] and self.visible == True:
+            Player.walkingRight = True
             Player.standing = False
-            Player.walking = True
-            Player.currentImage = Player.rightImage
+            Player.standingRight = True
             self.rect.x += Player.image_speed 
-            pos_x -= Player.image_speed 
+            pos_x -= Player.image_speed
         else:
-            Player.standing = True
-            Player.walking = False
-        #
+            Player.walkingRight = False
+        
         if self.rect.x < 0: # Simple Sides Collision
             self.rect.x = 0 # Reset Player Rect Coord
             pos_x = camera_pos[0] #Reset Camera Pos Coord
@@ -72,26 +76,30 @@ class Player:
             self.rect.y = 1980
             pos_y = camera_pos[1]
 
-        if Player.walking == True:
-            print("yes")
-        #
+        if Player.standingLeft == True or Player.standingRight == True:
+            Player.standing = True
+
+        if Player.walkingLeft == True or Player.walkingRight == True:
+            Player.walking = True
+        
         return (pos_x,pos_y) # Return New Camera Pos
+    
     def render(self,display):
         if self.visible == True:
-            Player.currentImage= pygame.transform.scale(Player.currentImage,(250,250))
-            display.blit(self.currentImage,(self.rect.x,self.rect.y))
-background = pygame.image.load("src\main/assets\elements/background\Background final2.jpg")
+            Player.currentSprite= pygame.transform.scale(Player.currentSprite,(250,250))
+            display.blit(self.currentSprite,(self.rect.x,self.rect.y))
+
+background = pygame.image.load("src\main/assets/textures\elements/background\placeholder_background_0.jpg")
 background= pygame.transform.scale(background,(2000,2000))
 
 def Main(display,clock):
     world = pygame.Surface((2000,2000)) # Create Map Surface
      
-    #
     player = Player() # Initialize Player Class
     camera_pos = (192,192) # Create Camara Starting Position 
     
     #values for animation calculation
-    value = 0
+    idleValue = 0
     WalkingValue = 0
 
     while True:
@@ -101,18 +109,25 @@ def Main(display,clock):
                 pygame.quit()
                 return
 
-        if value >= len(animations.idle_sprite):
-            value = 0
+        if idleValue >= len(animations.idle_sprite):
+            idleValue = 0
 
-        Player.currentImage = animations.idle_sprite[value]
+        Player.currentSprite = animations.idle_sprite[idleValue]
 
         if WalkingValue >= len(animations.walking_sprite):
             WalkingValue = 0
         
         camera_pos = player.move(camera_pos) # Run Player Move Function And Return New Camera Pos
 
-        if Player.walking == True:
-            Player.currentImage = animations.walking_sprite[WalkingValue]
+        #Player position detection
+        if Player.walkingRight == True:
+            Player.currentSprite = animations.walking_sprite[WalkingValue]
+            
+        if Player.walkingLeft == True:
+            Player.currentSprite = pygame.transform.flip(Player.currentSprite, True, False)
+
+        if Player.standingLeft == True:
+            Player.currentSprite = pygame.transform.flip(Player.currentSprite, True, False)
         
         player.render(world) # Render The Player
         display.fill(colors.WHITE) # Fill The Background White To Avoid Smearing
@@ -120,14 +135,14 @@ def Main(display,clock):
         world.blit(background, (0,0))
 
         if Player.standing == True:
-            value += 1
+            idleValue += 1
 
         if Player.walking == True:
             WalkingValue += 1
-        #
+        
         pygame.display.flip()
-###
-#
+
+
 if __name__ in "__main__":
     display = pygame.display.set_mode((1000,600), pygame.RESIZABLE)
     pygame.display.set_caption("CameraView")
---
+++ src/main/common/output.py
@@ -0,0 +1,9 @@
+import time
+
+class timeOutput:
+    now = time.gmtime()
+    #0 is the year
+    #1 is the month
+    #2 is the day
+    #3 
+    print(now[2])
