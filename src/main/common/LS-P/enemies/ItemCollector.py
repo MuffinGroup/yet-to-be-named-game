e23914d21a49ce0d50656284d1deb5971ecff982
ReadyPlayerOne14 <anitaseidl@outlook.de>
2023-03-21 15:00:46 +0100

    SomeChanges

--- src/main/common/LS-P/enemies/ItemCollector.py
+++ src/main/common/LS-P/enemies/ItemCollector.py
@@ -5,7 +5,7 @@ import random
 pygame.init()
 
 # set up screen
-screen_width = 800
+screen_width = 1000
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Controllable Character")
