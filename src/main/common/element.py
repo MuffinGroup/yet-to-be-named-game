633332137884730efe75309d386f6d61ba3c9ea7
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-03-23 17:22:19 +0100

    more progress on elementRegistry

--- src/main/common/element.py
+++ src/main/common/element.py
@@ -1,6 +1,13 @@
 import pygame
 
 pygame.init()
+class element():
+    def register_elements(self, elementCategory,elementName, registryName):
+        element.registryName = pygame.image.load("src/main/assets/textures/elements/environment/" + elementCategory + "/" + elementName + ".png")
 
-def register_elements():
-    
+    def blitElement(surface, elementX, elementY):
+        elementPos = (elementX, elementY)
+        surface.blit(element.registryName, elementPos)
+
+    def getRegistryName():
+        return element.registryName
