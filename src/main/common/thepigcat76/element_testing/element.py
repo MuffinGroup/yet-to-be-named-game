a66413f010e6dd2eb553f55de0f1bf733b4a5160
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-03-23 17:38:31 +0100

    more updates to elementRegistry

---
+++ src/main/common/thepigcat76/element_testing/element.py
@@ -0,0 +1,25 @@
+import pygame
+
+pygame.init()
+class self():
+    global registryName
+    global width
+    global height
+    def register_elements(self, elementCategory,elementName, registryName):
+        registryName = pygame.image.load("src/main/assets/textures/elements/environment/" + elementCategory + "/" + elementName + ".png")
+        registryName = pygame.transform.scale(registryName, (250,250))
+        width = registryName.get_width()
+        height = registryName.get_height()
+
+    def blitElement(surface, elementX, elementY):
+        elementPos = (elementX, elementY)
+        surface.blit(registryName, elementPos)
+
+    def getElementName():
+        return registryName
+    
+    def getElementWidth():
+        return width
+    
+    def getElementHeight():
+        return height
