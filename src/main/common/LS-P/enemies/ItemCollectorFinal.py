e31a3065c90b7da15fe6990b3c5a9fb9d4eb05f2
ReadyPlayerOne14 <anitaseidl@outlook.de>
2023-03-25 20:07:13 +0100

    Add Changes to ItemCollectorFinal

--- src/main/common/LS-P/enemies/ItemCollectorFinal.py
+++ src/main/common/LS-P/enemies/ItemCollectorFinal.py
@@ -2,97 +2,73 @@
 import pygame
 import random
 
-# Initialize Pygame
+# initialize pygame
 pygame.init()
-clock=pygame.time.Clock()
 
-# Define colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+# set up the game window
+window_width = 1200
+window_height = 700
+game_window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Collect Items Game")
 
-# Define screen dimensions
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 700
-
-# Create screen
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# Set window title
-pygame.display.set_caption("Collect Testing")
-
-# Load character image
-character_image = pygame.image.load("src\main/assets/textures\entities\characters\character_1/animations\character_1.png").convert_alpha()
+# set up the character and items
+character_image = pygame.image.load("src\main/assets/textures\entities\characters\character_1/animations\character_1.png")
 character_image=pygame.transform.scale(character_image,(250,250))
-screen.blit(character_image,(340,190))
+character_image.blit(character_image,(340,190))
 character_rect = character_image.get_rect()
+character_rect.center = (window_width//2, window_height//2)
+item_images = [pygame.image.load("src\main/assets/textures\entities\enemies\placeholder_enemy.png"), pygame.image.load("src\main/assets/textures\entities\enemies\placeholder_enemy - Kopie.png")]
+item_rects = []
+for i in range(1):
+    item_rect = item_images[i % len(item_images)].get_rect()
+    item_rect.center = (random.randint(50, window_width-50), random.randint(50, window_height-50))
+    item_rects.append(item_rect)
 
-# Load item image
-item_image = pygame.image.load("src\main/assets/textures\entities\enemies\placeholder_enemy.png").convert_alpha()
-item_image=pygame.transform.scale(item_image,(250,250))
-screen.blit(item_image,(340,190))
-item_rect = item_image.get_rect()
+# set up the font for the display
+font = pygame.font.SysFont(None, 30)
 
-#Starting position
-character_x = 50
-character_y = 50
-
-# Set initial position of character and item
-character_rect.centerx = SCREEN_WIDTH // 2
-character_rect.centery = SCREEN_HEIGHT // 2
-item_rect.centerx = random.randint(0, SCREEN_WIDTH)
-item_rect.centery = random.randint(0, SCREEN_HEIGHT)
-
-# Set initial score to zero
+# set up variables for the game loop
 score = 0
+game_running = True
 
-#Set character speed
-character_speed= 2
+# define a function to check if the character has collided with an item
+def check_collision(character_rect, item_rects):
+    for item_rect in item_rects:
+        if character_rect.colliderect(item_rect):
+            item_rects.remove(item_rect)
+            return True
+    return False
 
-# Create font object
-font = pygame.font.Font(None, 36)
-
-# Main game loop
-while True:
-    # Handle events
+# game loop
+while game_running:
+    # handle events
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE):
-            pygame.quit()
-            quit()
+        if event.type == pygame.QUIT:
+            game_running = False
     
-    
-    # Handle events
+    # handle key presses to move the character
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-     character_x -= character_speed
+        character_rect.move_ip(-1, 0)
     if keys[pygame.K_RIGHT]:
-     character_x += character_speed
+        character_rect.move_ip(1, 0)
     if keys[pygame.K_UP]:
-     character_y -= character_speed
+        character_rect.move_ip(0, -1)
     if keys[pygame.K_DOWN]:
-      character_y += character_speed
-
-    # Check if character is colliding with item
-    if character_rect.colliderect(item_rect):
-        # Update score
+        character_rect.move_ip(0, 1)
+    
+    # check for collision with items
+    if check_collision(character_rect, item_rects):
         score += 1
-        # Move item to new random position
-        item_rect.centerx = random.randint(0, SCREEN_WIDTH)
-        item_rect.centery = random.randint(0, SCREEN_HEIGHT)
+    
+    # draw the game
+    game_window.fill((255, 255, 255))
+    game_window.blit(character_image, character_rect)
+    for item_rect in item_rects:
+        game_window.blit(item_images[item_rects.index(item_rect) % len(item_images)], item_rect)
+    score_display = font.render("Score: {}".format(score), True, (0, 0, 0))
+    game_window.blit(score_display, (10, 10))
+    pygame.display.update()
 
-    # Fill screen with white color
-    screen.fill(WHITE)
-
-    # Draw character and item
-    screen.blit(character_image,(character_x, character_y), character_rect)
-    screen.blit(item_image, item_rect)
-
-    # Draw score on screen
-    score_text = font.render("Items Collected: {}".format(score), True, BLACK)
-    screen.blit(score_text, (10, 10))
-
-    # Update screen
-    pygame.display.flip()
-
-# Quit Pygame
+# quit pygame
 pygame.quit()
-
