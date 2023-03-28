5ecd385e308b447a6d0e8dde648fa6e6a915ceee
ReadyPlayerOne14 <anitaseidl@outlook.de>
2023-03-28 15:58:56 +0200

    Tests

--- src/main/common/LS-P/enemies/Test3.py
+++ src/main/common/LS-P/enemies/Test3.py
@@ -15,11 +15,11 @@ character_image = pygame.image.load("src\main/assets/textures\entities\character
 character_image=pygame.transform.scale(character_image,(250,250))
 character_image.blit(character_image,(340,190))
 character_rect = character_image.get_rect()
-character_rect.center = (window_width//2, window_height//2)
+character_rect.center = (window_width // 2, window_height // 2)
 
 item_images = [pygame.image.load("src\main/assets/textures\entities\enemies\placeholder_enemy.png"), pygame.image.load("src\main/assets/textures\entities\enemies\placeholder_enemy - Kopie.png")]
 item_rects = []
-for i in range(4):
+for i in range(10):
     item_rect = item_images[i % len(item_images)].get_rect()
     item_rect.center = (random.randint(50, window_width - 50), random.randint(50, window_height - 50))
     item_rects.append(item_rect)
@@ -33,6 +33,9 @@ inventory_rects = []  # the rects for displaying the inventory
 for i in range(4):
     rect = pygame.Rect(50 + i * 80, 10, 70, 70)
     inventory_rects.append(rect)
+
+item_counts = [0] * 4  # a list to store the count of items in each field of the inventory
+all_items_collected = False  # a flag for whether all items have been collected
 
 while True:
     # handle events
@@ -48,19 +51,22 @@ while True:
             for j in range(len(inventory)):
                 if inventory[j] is None:
                     inventory[j] = item_images[i % len(item_images)]
+                    item_counts[j] += 1
+                    if all(count == 1 for count in item_counts):
+                        all_items_collected = True
                     break
             item_rect.center = (random.randint(50, window_width - 50), random.randint(50, window_height - 50))
 
     # handle user input
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        character_rect.move_ip(-3, 0)
+        character_rect.move_ip(-5, 0)
     if keys[pygame.K_RIGHT]:
-        character_rect.move_ip(3, 0)
+        character_rect.move_ip(5, 0)
     if keys[pygame.K_UP]:
-        character_rect.move_ip(0, -3)
+        character_rect.move_ip(0, -5)
     if keys[pygame.K_DOWN]:
-        character_rect.move_ip(0, 3)
+        character_rect.move_ip(0, 5)
 
     # draw the graphics
     game_window.fill((255, 255, 255))
@@ -71,8 +77,15 @@ while True:
         if item is not None:
             inventory_rect = inventory_rects[i]
             game_window.blit(item, inventory_rect)
-    for i, inventory_rect in enumerate(inventory_rects):
-        pygame.draw.rect(game_window, (0, 0, 0), inventory_rect, 2)  # draw the outline of the inventory boxes
-    pygame.display.update()
+            count_text = font.render(str(item_counts[i]), True, (0, 0, 0))
+            count_rect = count_text.get_rect(center=inventory_rect.center)
+            game_window.blit(count_text, count_rect)
 
-pygame.quit()
+    # check if all items have been collected
+    if all_items_collected:
+     count_text = font.render(f"All items collected: {sum(item_counts)}", True, (0, 0, 0))
+     count_rect = count_text.get_rect(center=(window_width // 2, window_height - 30))
+     game_window.blit(count_text, count_rect)
+
+pygame.display.update()
+
