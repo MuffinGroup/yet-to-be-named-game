e8d663224c8bc358e31aed232fe23cfb424375f2
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-04-10 00:07:25 +0200

    important todo

--- src/main/common/thepigcat76/gui/main_gui.py
+++ src/main/common/thepigcat76/gui/main_gui.py
@@ -10,7 +10,7 @@ screen = pygame.display.set_mode((1280, 800), pygame.RESIZABLE)
 test = gui.registerGui(100, 100, 1000, 1000, True, "placeholder_startscreen")
 test2 = []
 test.registerRect(colors.DARK_ORANGE, 50, 50, 50, 69, 69420, test2)
-test.registerRect(colors.WHITE, 100, 100, 50, 69, 69420, test2)
+test.registerRect(colors.WHITE, 100, 100, 50, 69, 69420, test2) #both rects are test.rect which means they can't co-exist. Replace the self with something else and add a surface operator
 
 while True:
     for event in pygame.event.get():
