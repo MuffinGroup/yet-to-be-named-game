d5f34095e974eb251dc65f7c50883b5f251ec294
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-04-10 21:07:34 +0200

    Add support for more keys

--- src/main/common/thepigcat76/testing(69)/chat_test.py
+++ src/main/common/thepigcat76/testing(69)/chat_test.py
@@ -4,6 +4,7 @@ import pygame
 pygame.init()
 
 user_input = ""
+inputLocked = False
 
 #Load background
 background = pygame.image.load("src\main/assets/textures\elements/background\placeholder_background_1.jpg")
@@ -18,9 +19,15 @@ while True:
         if event.type == pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE):
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN:
-            user_input += event.unicode
+        if event.type == pygame.KEYDOWN and not inputLocked == True:
+            if event.key == pygame.K_BACKSPACE:
+                user_input = user_input[0:-1]
+            elif event.key == pygame.K_RETURN:
+                inputLocked = True
+            else:
+                user_input += event.unicode
             
+    screen.fill((0,0,0))
     text = textfont.render(user_input, True, (255, 255, 255))
     screen.blit(text, (330,310))
     pygame.display.update()
