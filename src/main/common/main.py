b67d7ab6e5d0beeeaceb925d3eab2c992f180135
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-04-24 16:04:06 +0200

    more changes

--- src/main/common/main.py
+++ src/main/common/main.py
@@ -375,7 +375,7 @@ def genWorld(world, map):
                 tile_rects.append(tileRect2)
                 world.blit(door_scaled, (x * dirtElementScaled.get_width(), y * dirtElementScaled.get_width()))
                 if Player.debuggingMode == True:
-                    pygame.draw.rect(world, (255, 255, 255), tileRect, 2)
+                    pygame.draw.rect(world, (255, 255, 255), tileRect2, 2)
             x += 1
         y += 1
         
