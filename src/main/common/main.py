187f04292c9009b028a5651974701588675fdc9d
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-04-24 21:48:44 +0200

    started work on ctm

--- src/main/common/main.py
+++ src/main/common/main.py
@@ -340,7 +340,10 @@ def genWorld(world, map):
                 if Player.debuggingMode == True:
                     pygame.draw.rect(world, (255, 255, 255), tileRect, 2)
             if tile == 3:
-                world.blit(cobbleElementScaled, (x * dirtElementScaled.get_width(), y * dirtElementScaled.get_width()))
+                if map[11][tile + 1] == 0:
+                    print("uwu")
+                else:
+                    world.blit(cobbleElementScaled, (x * dirtElementScaled.get_width(), y * dirtElementScaled.get_width()))
                 if Player.debuggingMode == True:
                     pygame.draw.rect(world, (255, 255, 255), tileRect, 2)
             if tile == 4:
