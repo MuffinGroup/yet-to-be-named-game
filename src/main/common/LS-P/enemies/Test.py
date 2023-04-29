14fc667024b203600ca9e2080d34d903fad316fe
ReadyPlayerOne14 <anitaseidl@outlook.de>
2023-04-29 21:18:00 +0200

    New Code

--- src/main/common/LS-P/enemies/Test.py
+++ src/main/common/LS-P/enemies/Test.py
@@ -1,19 +1,50 @@
-import socket
+import cv2
 
-s = socket.socket()
+# Load the two images
+image1 = cv2.imread('src\main/assets/textures\entities\characters\character_1/animations\character_1.png')
+image2 = cv2.imread('src\main/assets/textures\entities\enemies\placeholder_enemy.png')
 
-s.bind(('localhost', 1234))
+# Create a mask for the first image
+gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(gray1, 10, 255, cv2.THRESH_BINARY)
 
-s.listen(5)
+# Set up the initial position of the second image
+pos_x = 0
+pos_y = 0
+
+# Define a function to handle mouse events
+def mouse_callback(event, x, y, flags, param):
+    global pos_x, pos_y
+    if event == cv2.EVENT_MOUSEMOVE:
+        pos_x = x
+        pos_y = y
+
+# Create a window and set the mouse callback function
+cv2.namedWindow('Image')
+cv2.setMouseCallback('Image', mouse_callback)
 
 while True:
+    # Create a copy of the second image
+    overlay = image2.copy()
 
-  c, addr = s.accept()
+    # Calculate the new position of the second image
+    new_x = pos_x - image1.shape[1] // 2
+    new_y = pos_y - image1.shape[0] // 2
 
-  print ("Got connection from"), 
-  addr
+    # Apply the mask to the first image
+    masked_image = cv2.bitwise_and(image1, image1, mask=mask)
 
-c.send("Thank you for connecting")
+    # Overlay the masked image onto the second image
+    overlay[new_y:new_y+masked_image.shape[0], new_x:new_x+masked_image.shape[1]] = masked_image
 
-c.close()
+    # Display the result
+    cv2.imshow('Image', overlay)
+
+    # Check for key presses
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+# Clean up
+cv2.destroyAllWindows()
 
