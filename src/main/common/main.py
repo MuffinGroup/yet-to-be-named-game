6170d4838a6f68cea47bb93b893579630bdf28a9
ReadyPlayerOne14 <anitaseidl@outlook.de>
2023-05-01 11:12:13 +0200

    Test

--- src/main/common/main.py
+++ src/main/common/main.py
@@ -661,11 +661,10 @@ def Tut1(language):
         enemy_img = pygame.image.load("src\main/assets/textures\entities\enemies\placeholder_enemy.png")
         enemy_img_Scaled = pygame.transform.scale(enemy_img,(enemy_img.get_width() * 8, enemy_img.get_width() * 8))
         enemy_speed = 5
+        enemy_facing_left = True
         enemy_x -= enemy_speed
         if enemy_x > 2050:
            enemy_x -= enemy_speed
-        if enemy_x < 700:
-           enemy_x += enemy_speed
         world.blit(enemy_img_Scaled,(enemy_x, enemy_y))
         
         
