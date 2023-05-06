9b0a651eaf8774ebec4b3241ed933c825893a1cb
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-06 19:26:18 +0200

    cleaned directories and started work on alert api

---
+++ src/main/common/thepigcat76/testing(69)/alerts.py
@@ -0,0 +1,15 @@
+import pygame
+
+class notification:
+    def __init__():
+        pass
+
+    def render():
+        pass
+
+class infoToast:
+    def __init__(self):
+        pass
+
+    def render(self):
+        pass
