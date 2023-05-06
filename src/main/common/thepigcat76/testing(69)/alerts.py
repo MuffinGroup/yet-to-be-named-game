0a66919b68f24a6ec24641d74fa05ea5e572bf96
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-06 19:52:27 +0200

    added notification bar

--- src/main/common/thepigcat76/testing(69)/alerts.py
+++ src/main/common/thepigcat76/testing(69)/alerts.py
@@ -1,11 +1,13 @@
 import pygame
 
 class notification:
-    def __init__():
-        pass
+    def __init__(self, path, startX, startY, scale):
+        self.notification_bar = pygame.image.load(path)
+        self.scaledTexture = pygame.transform.scale(self.notification_bar, (self.notification_bar.get_width() * scale, self.notification_bar.get_height() * scale))
+        self.startX, self.startY = startX, startY
 
-    def render():
-        pass
+    def render(self, surface):
+        surface.blit(self.scaledTexture, (self.startX, self.startY))
 
 class infoToast:
     def __init__(self):
