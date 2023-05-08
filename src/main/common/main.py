7e90a7cede9f750ef61d7c9d35ede76b3efb369e
ReadyPlayerOne14 <anitaseidl@outlook.de>
2023-05-08 15:55:25 +0200

    Start Screen as first

--- src/main/common/main.py
+++ src/main/common/main.py
@@ -1332,4 +1332,4 @@ if __name__ in "__main__":
     pygame.display.set_caption("yet-to-be-named-game")
     pygame.display.set_icon(icon)
     clock = pygame.time.Clock()
-    Tut1(Player.language) #this isn't start bc i need to do some debugging and testing
+    Start(Player.language) #this isn't start bc i need to do some debugging and testing
