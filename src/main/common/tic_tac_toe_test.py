8dac356a8c204a259c574e8bec83cf286cb7f557
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-18 20:07:01 +0200

    I hate this tic tac toe bot

--- src/main/common/tic_tac_toe_test.py
+++ src/main/common/tic_tac_toe_test.py
@@ -10,6 +10,7 @@ ttt_map = [[0, 0, 0],
            [0, 0, 0]]
 
 inputLocked = False
+input = 1
 
 while True:
     for event in pygame.event.get():
@@ -17,34 +18,36 @@ while True:
             pygame.quit()
             exit()
 
+    frameX, frameY = random.randint(0, 2), random.randint(0, 2)
+
     y = 0
     for row in ttt_map:
         x = 0
         for tile in row:
             frame = pygame.Rect((x * 100, y * 100), (100, 100))
             if tile == 0:
-                pygame.draw.rect(screen, (255, 255, 255), frame, 3)
-            if frame.collidepoint(pygame.mouse.get_pos()):
-                pygame.draw.rect(screen, (111, 0, 69), frame, 3)
+                if frame.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] == 1 and inputLocked == False:
+                    ttt_map[frame.y//100][frame.x//100] = 1
+                    inputLocked = True
+                else:
+                    pygame.draw.rect(screen, (255, 255, 255), frame, 3)
             if tile == 1:
-                pygame.draw.rect(screen, (0, 0, 255), frame, 3)
+                pygame.draw.rect(screen, (255, 0, 255), frame, 3)
             if tile == 2:
                 pygame.draw.rect(screen, (255, 0, 0), frame, 3)
-            if frame.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] == 1:
-                ttt_map[frame.y//100][frame.x//100] = 1
-            if all(cell == 1 for cell in row):
-                print("player won!")
-            for col in range(3):
-                if all(ttt_map[row][col] == 1 for row in range(3)):
-                    print("player won!")
 
+            if inputLocked == True:
+                if ttt_map[frameX][frameY]:
+                    ttt_map[frameX][frameY] = 2
+                    inputLocked = False
+                    
             # Check diagonals
             if all(ttt_map[i][i] == 1 for i in range(3)):
                 print("player won!")
             if all(ttt_map[i][2 - i] == 1 for i in range(3)):
-                print("player won!")      
+                print("player won!")
             x += 1
         y += 1
 
-    print(ttt_map)
+    print(inputLocked)
     pygame.display.update()
