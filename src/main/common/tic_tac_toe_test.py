075b709497201f7325fdcf3f784f964f0f452b77
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-18 21:00:08 +0200

    fully finished the game

--- src/main/common/tic_tac_toe_test.py
+++ src/main/common/tic_tac_toe_test.py
@@ -13,6 +13,9 @@ ttt_map = [[0, 0, 0],
 inputLocked = False
 input = 1
 gameWon = False
+gameLost = False
+loseTimer = 0
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
@@ -34,16 +37,19 @@ while True:
                 else:
                     pygame.draw.rect(screen, (255, 255, 255), frame, 3)
             if tile == 1:
-                pygame.draw.rect(screen, (255, 0, 255), frame, 3)
+                pygame.draw.line(screen, (255, 0, 0), frame.bottomleft, frame.topright, 3)
+                pygame.draw.line(screen, (255, 0, 0), frame.bottomright, frame.topleft, 3)
             if tile == 2:
-                pygame.draw.rect(screen, (255, 0, 0), frame, 3)
+                pygame.draw.circle(screen, (255, 0, 255), frame.center, frame.width//2, 7)
 
             if inputLocked == True:
                 if ttt_map[frameX][frameY] == 0:
+                    pygame.time.wait(200)
                     ttt_map[frameX][frameY] = 2
                     inputLocked = False
 
-                if all(cell == 1 for cell in row):
+            #Check lines
+            if all(cell == 1 for cell in row):
                     gameWon = True
 
             # Check columns
@@ -57,13 +63,40 @@ while True:
 
             if all(ttt_map[i][2 - i] == 1 for i in range(3)):
                     gameWon = True
+
+
+            #Bot winning
+            #Check lines
+            if all(cell == 2 for cell in row):
+                    gameLost = True
+
+            # Check columns
+            for col in range(3):
+                if all(ttt_map[row][col] == 2 for row in range(3)):
+                    gameLost = True
+
+            # Check diagonals
+            if all(ttt_map[i][i] == 2 for i in range(3)):
+                    gameLost = True
             
+            if all(ttt_map[i][2 - i] == 2 for i in range(3)):
+                    gameLost = True
+    
             if gameWon == True:
-                print("Player wins")
+                print("Player wins!")
                 pygame.quit()
                 sys.exit()
+
+            if gameLost == True:
+                if loseTimer < 180:
+                     loseTimer += 1
+                else:
+                    print("Bot wins!")
+                    pygame.quit()
+                    sys.exit()
 
             x += 1
         y += 1
 
+    clock.tick(100)
     pygame.display.update()
